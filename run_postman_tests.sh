f056d0cbd02daa4dd20b17aea5dc17fa8a362013
andrewlarsen <andrew.larsen@concordusa.com>
2017-11-05 18:44:57 -0600

    fix name?

--- run_postman_tests.sh
+++ run_postman_tests.sh
@@ -3,4 +3,4 @@
 ENV=$1
 #Get output values, this is a somewhat naive approach since it is a lot of api calls
 
-newman run integration/moodle_users.postman_collection.json -e "moodle_users_${ENV}.postman_environment.json" --timeout-request 15000
+newman run integration/moodle_users.postman_collection.json -e moodle_users_${ENV}.postman_environment.json --timeout-request 15000
