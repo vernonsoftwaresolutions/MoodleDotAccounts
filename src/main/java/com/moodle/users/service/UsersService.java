7d77d0d5d85b264f47cf8c0ac4466c8b2cdabeaa
andrewlarsen <andrew.larsen@concordusa.com>
2017-11-09 21:57:43 -0600

    add request to put moodle tenant request on the queue

--- src/main/java/com/moodle/users/service/UsersService.java
+++ src/main/java/com/moodle/users/service/UsersService.java
@@ -1,22 +1,36 @@
 package com.moodle.users.service;
 
+import com.moodle.users.client.MoodleTenantClient;
 import com.moodle.users.model.User;
 import com.moodle.users.model.UserDTO;
+import com.moodle.users.model.moodle.MoodleTenantRequest;
+import com.moodle.users.model.moodle.SQSResponse;
 import com.moodle.users.repository.UsersRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 /**
  * Created by andrewlarsen on 11/4/17.
  */
 @Service
 public class UsersService {
+    private static final String STACK_NAME = "moodle-ecs-single";
+    private static final String VPC_ID = "vpc-c7aa77be";
+    private static final String HOSTED_ZONE = "vssdevelopment.com";
+    private Logger log = LoggerFactory.getLogger(UsersService.class);
 
     private UsersRepository usersRepository;
+    private MoodleTenantClient tenantClient;
+
 
     @Autowired
-    public UsersService(UsersRepository usersRepository) {
+    public UsersService(UsersRepository usersRepository, MoodleTenantClient tenantClient) {
         this.usersRepository = usersRepository;
+        this.tenantClient = tenantClient;
     }
 
     /**
@@ -25,7 +39,15 @@ public class UsersService {
      * @return
      */
     public User save(UserDTO user){
-        return usersRepository.save(createUser(user));
+        //first save the user into the user database
+        log.info("Saving user to db");
+        User savedUser = usersRepository.save(createUser(user));
+        //next put the message on the queue
+        //todo-need to address error situations
+        SQSResponse response = tenantClient.postMessage(createRequest(user));
+
+        return savedUser;
+
     }
 
     /**
@@ -41,6 +63,25 @@ public class UsersService {
         user.setPhoneNumber(userDTO.getPhoneNumber());
         user.setCompanyName(userDTO.getCompanyName());
         return user;
+    }
+
+    /**
+     * Helper method to create Moodle Tenant Request
+     * @param userDTO
+     * @return
+     */
+    private MoodleTenantRequest createRequest(UserDTO userDTO){
+        Random rand = new Random();
+
+        int priority = rand.nextInt(100) + 1;
+        MoodleTenantRequest request = new MoodleTenantRequest();
+        request.setClientName(userDTO.getCompanyName());
+        request.setPriority(priority);
+        request.setHostedZoneName(HOSTED_ZONE);
+        request.setStackName(STACK_NAME);
+        request.setVpcId(VPC_ID);
+
+        return request;
     }
 
 }
--- src/test/java/com/moodle/users/service/UsersServiceTest.java
+++ src/test/java/com/moodle/users/service/UsersServiceTest.java
@@ -1,7 +1,9 @@
 package com.moodle.users.service;
 
+import com.moodle.users.client.MoodleTenantClient;
 import com.moodle.users.model.User;
 import com.moodle.users.model.UserDTO;
+import com.moodle.users.model.moodle.SQSResponse;
 import com.moodle.users.repository.UsersRepository;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +12,7 @@ import org.mockito.MockitoAnnotations;
 
 import static org.junit.Assert.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyObject;
 
 /**
@@ -19,25 +22,28 @@ public class UsersServiceTest {
 
     @Mock
     private UsersRepository usersRepository;
+    @Mock
+    private MoodleTenantClient client;
     private UsersService service;
     private UserDTO userDTO;
     private User user;
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
-        service = new UsersService(usersRepository);
+        service = new UsersService(usersRepository,client);
         userDTO = new UserDTO();
 
     }
     @Test
     public void save() throws Exception {
         given(usersRepository.save(anyObject())).willReturn(user);
+        given(client.postMessage(any())).willReturn(new SQSResponse());
         assertEquals(service.save(userDTO),user) ;
     }
     @Test
     public void saveCompanyName() throws Exception {
         given(usersRepository.save(anyObject())).willReturn(user);
-
+        given(client.postMessage(any())).willReturn(new SQSResponse());
         assertEquals(service.save(userDTO),user) ;
     }
 }
