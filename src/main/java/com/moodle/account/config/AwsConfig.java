f23c7c6908c0ebb88fb495ad8d505d1b44fc91dc
andrewlarsen <andrew.larsen@concordusa.com>
2018-02-11 21:35:27 -0600

    add @Configuration annotation

--- src/main/java/com/moodle/account/config/AwsConfig.java
+++ src/main/java/com/moodle/account/config/AwsConfig.java
@@ -3,10 +3,12 @@ package com.moodle.account.config;
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClientBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by andrewlarsen on 2/11/18.
  */
+@Configuration
 public class AwsConfig {
     @Bean
     public AWSCognitoIdentityProvider awsCognitoIdentityProvider(){
